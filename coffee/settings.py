eb1825ddfe3223119a08fe46752312e36d875a19
kathyzwq <wzywdq@163.com>
2023-08-26 20:07:35 +0800

    0826.8

--- coffee/settings.py
+++ coffee/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=545+@#2xhiqt6wye=h&y@bel_a60i7pwj9h58%ph*2fa8h5(+'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
